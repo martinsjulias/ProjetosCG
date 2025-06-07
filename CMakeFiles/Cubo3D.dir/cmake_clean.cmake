a024c82595cb138a9c7f43808a5dbf9581942307
martinsjulias <juliamartins240903@gmail.com>
2025-06-07 16:45:05 -0300

    commit arquivos resposta do desafio módulo2

---
+++ CMakeFiles/Cubo3D.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cubo3D.dir/common/glad.c.obj"
+  "CMakeFiles/Cubo3D.dir/common/glad.c.obj.d"
+  "CMakeFiles/Cubo3D.dir/src/Cubo3D.cpp.obj"
+  "CMakeFiles/Cubo3D.dir/src/Cubo3D.cpp.obj.d"
+  "Cubo3D.exe"
+  "Cubo3D.exe.manifest"
+  "Cubo3D.pdb"
+  "libCubo3D.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Cubo3D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
